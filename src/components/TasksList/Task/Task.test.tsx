2ca9b1f880646ce2d2ec08566bdc329da505eabe
Manu Hdez <manu.hdez.dev@gmail.com>
2020-09-14 22:16:48 +0100

    Task component

---
+++ src/components/TasksList/Task/Task.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
+
+import Task, { TaskProps } from './Task';
+
+global.fetch = jest.fn();
+
+describe('Task', () => {
+  let mockProps: TaskProps;
+  beforeEach(() => {
+    (global.fetch as jest.Mock).mockClear();
+
+    mockProps = {
+      id: 32,
+      title: 'Feed the cat',
+      createdAt: 1600035373810,
+      done: false,
+      onUpdatedTask: jest.fn(),
+    };
+  });
+
+  test('Displays the title of the task and its done status', () => {
+    render(<Task {...mockProps} />);
+
+    expect(screen.getByText('Feed the cat')).toBeInTheDocument();
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+  });
+
+  test('Marks the task as done after a user clicks on the checkbox', () => {
+    render(<Task {...mockProps} />);
+
+    const checkbox = screen.getByRole('checkbox');
+    expect(global.fetch).not.toHaveBeenCalled();
+
+    fireEvent.click(checkbox);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test('User can edit a task', () => {
+    render(<Task {...mockProps} />);
+
+    const editButton = screen.getByTitle('Edit');
+    expect(editButton).toBeInTheDocument();
+
+    fireEvent.click(editButton);
+
+    const cancelButton = screen.getByTitle('Cancel');
+    const saveButton = screen.getByTitle('Save');
+
+    expect(cancelButton).toBeInTheDocument();
+    expect(saveButton).toBeInTheDocument();
+    expect(global.fetch).toHaveBeenCalledTimes(0);
+
+    fireEvent.click(saveButton);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/components/TasksList/Task/Task.tsx
@@ -0,0 +1,130 @@
+import useFetch from 'hooks/useFetch';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
+import styles from './Task.module.scss';
+
+export interface TaskData {
+  title: string;
+  done: boolean;
+  // createdAt saved as a timestamp
+  createdAt: number;
+}
+
+export interface TaskRecord extends TaskData {
+  id: number;
+}
+
+export interface TaskProps extends TaskRecord {
+  onUpdatedTask: () => Promise<void>;
+}
+
+export default function Task(props: TaskProps) {
+  const { id, title, done, createdAt, onUpdatedTask } = props;
+
+  const [editingTask, setEditingTask] = useState(false);
+
+  const editInputRef = React.createRef<HTMLInputElement>();
+
+  const [updateTask, , isUpdatingTask, errorUpdatingTask] = useFetch<
+    TaskRecord,
+    TaskData
+  >({ url: `/tasks/${id}`, method: 'PUT' });
+
+  const [removeTask, , , errorRemovingTask] = useFetch<TaskRecord>({
+    url: `/tasks/${id}`,
+    method: 'DELETE',
+  });
+
+  const handleToggleTask = async (e: ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    await updateTask({ done: checked, title, createdAt });
+
+    if (!errorUpdatingTask) {
+      onUpdatedTask();
+    }
+  };
+
+  const handleSaveUpdatedTask = async () => {
+    const newTitle = editInputRef.current?.value;
+    // should show a validation message
+    if (!newTitle) return;
+
+    await updateTask({ done, title: newTitle, createdAt });
+    if (!errorUpdatingTask) {
+      await onUpdatedTask();
+      setEditingTask(false);
+    }
+  };
+
+  const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSaveUpdatedTask();
+    }
+  };
+
+  const handleRemoveTask = async () => {
+    await removeTask();
+    if (!errorRemovingTask) {
+      onUpdatedTask();
+    }
+  };
+
+  const renderEditingTask = () => {
+    return (
+      <div className={styles.content}>
+        <input
+          type="text"
+          defaultValue={title}
+          ref={editInputRef}
+          onKeyUp={handleKeyUp}
+        />
+        <div className={styles.actions}>
+          <button onClick={() => setEditingTask(!editingTask)}>
+            <span role="img" aria-label="Cancel" title="Cancel">
+              ❌
+            </span>
+          </button>
+          <button onClick={handleSaveUpdatedTask}>
+            <span role="img" aria-label="Save" title="Save">
+              💾
+            </span>
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  const renderTaskContent = () => {
+    return (
+      <div className={`${styles.content} ${done && styles.done}`}>
+        <p>{title}</p>
+        <div className={styles.actions}>
+          {done ? (
+            <button onClick={handleRemoveTask}>
+              <span role="img" aria-label="Remove" title="Remove task">
+                🗑
+              </span>
+            </button>
+          ) : (
+            <button onClick={() => setEditingTask(!editingTask)}>
+              <span role="img" aria-label="Edit" title="Edit">
+                ✏️
+              </span>
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <li className={styles.task}>
+      <input
+        type="checkbox"
+        checked={done}
+        disabled={isUpdatingTask}
+        onChange={handleToggleTask}
+      />
+      {editingTask ? renderEditingTask() : renderTaskContent()}
+    </li>
+  );
+}
